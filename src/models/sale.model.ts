3b047647246549f0bb3bc3bdf0f18ac4d8ee1d69
badaruddinomar <badaruddinomar2403@gmail.com>
2024-12-26 14:26:44 +0600

    add: sale schema, remove: customer schema

---
+++ src/models/sale.model.ts
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+
+const saleSchema = new mongoose.Schema(
+  {
+    customerName: {
+      type: String,
+      required: [true, 'Customer name is required'],
+    },
+    customerPhone: {
+      type: String,
+      required: [true, 'Customer phone is required'],
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        unit: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Unit',
+          required: true,
+        },
+        salePrice: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalAmount: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ['Cash', 'Card', 'UPI', 'Credit'],
+      default: 'Cash',
+    },
+    saleDate: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+const Sale = mongoose.model('Sale', saleSchema);
+export default Sale;
